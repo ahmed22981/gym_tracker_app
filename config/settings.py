686f0c2482b1f7df6d6ad38c2b55655d32f69bd3
Ahmed Hatem <ahmedhatemomran@gmail.com>
2026-05-19 16:54:28 +0300

    fix: restore STATICFILES_STORAGE to resolve cloudinary-storage library build error

--- config/settings.py
+++ config/settings.py
@@ -142,6 +142,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
