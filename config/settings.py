44b01048b94cf98e7ce848ec476c1960b465ca21
Ahmed Hatem <ahmedhatemomran@gmail.com>
2026-05-19 17:15:32 +0300

    fix: remove manifest from STORAGES backend to finally bypass whitenoise crash

--- config/settings.py
+++ config/settings.py
@@ -155,9 +155,8 @@ STORAGES = {
     "default": {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage"
     },
-    
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage"
     }
 }
 
