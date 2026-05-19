199fcec085715e540cfb33ec434c4069525c9518
Ahmed Hatem <ahmedhatemomran@gmail.com>
2026-05-19 17:08:39 +0300

    fix: switch to CompressedStaticFilesStorage to bypass whitenoise missing file crash

--- config/settings.py
+++ config/settings.py
@@ -142,8 +142,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 WHITENOISE_MANIFEST_STRICT = False
 
 CLOUDINARY_STORAGE = {
